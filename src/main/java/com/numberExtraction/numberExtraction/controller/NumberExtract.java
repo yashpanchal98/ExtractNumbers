dc2c874e6a28930790e33dc7de4527a5f705ec6b
yashpanchal98 <pclyash@gmail.com>
2025-09-20 23:32:23 +0530

    Get Request fixed

--- src/main/java/com/numberExtraction/numberExtraction/controller/NumberExtract.java
+++ src/main/java/com/numberExtraction/numberExtraction/controller/NumberExtract.java
@@ -1,10 +1,7 @@
 package com.numberExtraction.numberExtraction.controller;
 
 import com.numberExtraction.numberExtraction.InputString;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -12,12 +9,28 @@ import java.util.*;
 @RequestMapping("/api")
 public class NumberExtract {
 
+    // Store all previous POST results
+    private static final List<List<Integer>> allResults = new ArrayList<>();
+
+    // POST mapping (your working one)
     @PostMapping("/extractNumbers")
     public List<Integer> getNumbers(@RequestBody InputString inputString) {
+        List<Integer> numbers = extractNumbers(inputString.getText());
+        allResults.add(numbers); // store result
+        return numbers;
+    }
+
+    // GET mapping to return all previous POST results
+    @GetMapping("/allNumbers")
+    public List<List<Integer>> getAllNumbers() {
+        return allResults;
+    }
+
+    // Helper method to extract numbers from a string
+    private List<Integer> extractNumbers(String text) {
         List<Integer> numbers = new ArrayList<>();
         String temp = "";
-
-        for (char c : inputString.getText().toCharArray()) {
+        for (char c : text.toCharArray()) {
             if (Character.isDigit(c)) {
                 temp += c;
             } else {
@@ -30,7 +43,6 @@ public class NumberExtract {
         if (!temp.isEmpty()) {
             numbers.add(Integer.parseInt(temp));
         }
-
         return numbers;
     }
 }
